1d5140cb55bb828c305527d90a7184e7d3775a6e
energos <energos@users.noreply.github.com>
2024-11-29 12:21:19 -0300

    Emacs: Initial frame width. The ugly gets uglier.

--- dotfiles/home/.emacs.d/init.el
+++ dotfiles/home/.emacs.d/init.el
@@ -8,8 +8,11 @@
 ;; resize initial frame to 1/2 screen width
 ;; temporary ugly hack
 (sleep-for 0.1)
-(set-frame-width nil (if (= (default-font-width) 11) 173 146))
-(set-frame-parameter nil 'energos/width 10)
+(if (= (default-font-width) 11)
+    (progn (set-frame-width nil 173)
+           (set-frame-parameter nil 'energos/width 10))
+  (set-frame-width nil 137)
+  (set-frame-parameter nil 'energos/width 9))
 
 (desktop-save-mode)                     ; restore desktop, except
 (setq desktop-restore-frames nil)       ; for window and frame configuration
